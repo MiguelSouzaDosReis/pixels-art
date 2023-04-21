b7250a8252601c294d539b631dff2b181faeb390
Miguel Reis <guelsouzar@gmail.com>
2023-04-21 11:15:07 -0300

    feat: add clear

--- script.js
+++ script.js
@@ -1,65 +1,72 @@
-// fiz a tabela baseada nesse video https://www.youtube.com/watch?v=-dGWaHA3ud8
-let botao = document.querySelectorAll('button')
-let color = document.querySelectorAll('.color')
-let primeiraCor = color[0] // pegando o primeiro color
-let selected = document.querySelector('.selected')
-let Preto = document.getElementById('PRETO')
-let Azul = document.getElementById('AZUL')
-let Vermelho = document.getElementById('VERMELHO')
-let Amarelo = document.getElementById('AMARELO')
+let color = document.querySelectorAll(".color");
+let firstColor = color[0]; // pegando o primeiro color
+let selected = document.querySelector(".selected");
+let black = document.getElementById("PRETO");
+let blue = document.getElementById("AZUL");
+let red = document.getElementById("VERMELHO");
+let yellow = document.getElementById("AMARELO");
 
-
-
-
-function criarLinhas() {
+function createLines() {
   for (let index = 0; index < 5; index++) {
-    const tabela = document.getElementById('pixel-board');
-    const divs = document.createElement('div');
+    const tabela = document.getElementById("pixel-board");
+    const divs = document.createElement("div");
     tabela.appendChild(divs);
 
     for (let i = 0; i < 5; i++) {
-      const sessao = document.createElement('div');
-      sessao.classList.add('pixel');
+      const sessao = document.createElement("div");
+      sessao.classList.add("pixel");
       divs.appendChild(sessao);
     }
   }
 }
 
-function selecionar() {
+function select() {
+  let color = document.querySelectorAll(".color");
+  let firstColor = color[0];
 
   for (let index = 0; index < color.length; index++) {
-    color[index].addEventListener('click', function (event) {
-      primeiraCor.classList.remove('selected')
-      event.target.classList.add('selected')
-      primeiraCor = event.target
-    })
+    color[index].addEventListener("click", function (event) {
+      firstColor.classList.remove("selected");
+      event.target.classList.add("selected");
+      firstColor = event.target;
+    });
   }
-
 }
+
 function pixelColor() {
-  let pixel = document.querySelectorAll('.pixel')
+  let pixel = document.querySelectorAll(".pixel");
+
   for (let index = 0; index < pixel.length; index++) {
-
-    pixel[index].addEventListener('click', function () {
-      pixel[index].className = 'pixel'
-      if (Preto.classList.contains('selected') === true) {
-
-        pixel[index].classList.add('preto')
-
+    pixel[index].addEventListener("click", function () {
+      if (black.classList.contains("selected")) {
+        pixel[index].style.backgroundColor = "black";
+        pixel[index].style.mixBlendMode = "multiply";
+      } else if (yellow.classList.contains("selected")) {
+        pixel[index].style.backgroundColor = "yellow";
+        pixel[index].style.mixBlendMode = "initial";
+      } else if (red.classList.contains("selected")) {
+        pixel[index].style.backgroundColor = "red";
+        pixel[index].style.mixBlendMode = "initial";
+      } else if (blue.classList.contains("selected")) {
+        pixel[index].style.backgroundColor = "blue";
+        pixel[index].style.mixBlendMode = "initial";
       }
-      else if (Amarelo.classList.contains('selected') === true) {
-        pixel[index].classList.add('amarelo')
-      }
-      else if (Vermelho.classList.contains('selected') === true) {
-        pixel[index].classList.add('vermelho')
-      }
-      else if (Azul.classList.contains('selected') === true) {
-        pixel[index].classList.add('azul')
-        console.log('azul')
-      }
-    })
+    });
   }
 }
-criarLinhas()
-selecionar()
-pixelColor()
+
+function clean() {
+  let pixel = document.querySelectorAll(".pixel");
+
+  for (let index = 0; index < pixel.length; index++) {
+    pixel[index].style.backgroundColor = "white";
+    pixel[index].style.mixBlendMode = "initial";
+  }
+}
+
+createLines();
+select();
+pixelColor();
+
+const buttonClean = document.getElementById("limpar");
+buttonClean.addEventListener("click", clean);
